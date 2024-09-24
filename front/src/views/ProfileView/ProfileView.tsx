699e8ee4b6659a792893b94c2f0b5dde3b7a3bdf
lucasmzd <ilucasmzd@gmail.com>
2024-09-24 11:42:19 -0300

    Dashboard data

--- front/src/views/ProfileView/ProfileView.tsx
+++ front/src/views/ProfileView/ProfileView.tsx
@@ -1,11 +1,18 @@
 "use client";
+import { IUserSession } from "@/interfaces/types";
 import { useRouter } from "next/navigation";
-// import { Router } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
 const ProfileView = () => {
   const router = useRouter()
+  const [userData, setUserData] = useState<IUserSession>();
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const userData = JSON.parse(localStorage.getItem("userSession")!);
+      setUserData(userData);
+    }
+  }, []);
   const handleClick = () => {
     localStorage.removeItem("userSession");
     Swal.fire({
@@ -17,7 +24,10 @@ const ProfileView = () => {
   return (
     <div>
       <h1>Profile</h1>
-      <h3>Bienvenido Usuario</h3>
+      <h3>Bienvenido {userData?.user.name}</h3>
+      <p>Email: {userData?.user.email}</p>
+      <p>Phone: {userData?.user.phone}</p>
+      <p>Address: {userData?.user.address}</p>
       <button onClick={handleClick} disabled={false}>Logout</button>
     </div>
   );
