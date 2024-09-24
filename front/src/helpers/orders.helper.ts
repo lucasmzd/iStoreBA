9f43988d7ecdda7ec72fe67ecec9f1fbf14be839
lucasmzd <ilucasmzd@gmail.com>
2024-09-24 18:58:52 -0300

    Order view, order creator, alert y empty cart

--- front/src/helpers/orders.helper.ts
+++ front/src/helpers/orders.helper.ts
@@ -1,24 +1,20 @@
-// import { IProduct } from "@/interfaces/types";
+const APIURL = process.env.NEXT_PUBLIC_API_URL;
 
-// const APIURL = process.env.NEXT_PUBLIC_API_URL;
-
-// export async function getProductsDB(): Promise<IProduct[]> {
-//     try {
-//         const res = await fetch(`${APIURL}/products`,{next: {revalidate: 1200}})
-//         const products: IProduct[] = await res.json();
-//         return products
-//     } catch (error: any) {
-//         throw new Error(error);
-//     }
-// }
-
-// export async function getProductsById(id: string): Promise<IProduct[]> {
-//     try {
-//         const products: IProduct[] = await getProductsDB();
-//         const productFiltered = products.find((product) => product.id.toString() === id);
-//         if (!productFiltered) throw new Error("Product not found");
-//         return productFiltered;
-//     } catch (error: any) {
-//         throw new Error(error);
-//     }
-// }
+export async function createOrder(products:number[], token:string) {
+    try {
+        const res = await fetch(`${APIURL}/orders`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                Authorization: token
+            },
+            body: JSON.stringify({
+                products
+            })
+        })
+        const orders = await res.json()
+        return orders
+    } catch (error: any) {
+        throw new Error(error);
+    }
+}
--- front/src/views/CartView/CartView.tsx
+++ front/src/views/CartView/CartView.tsx
@@ -1,7 +1,72 @@
-import React from "react";
+"use client";
+import { createOrder } from "@/helpers/orders.helper";
+import { IProduct, IUserSession } from "@/interfaces/types";
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 
 const CartView = () => {
-  return <div>CartView</div>;
+  const [cart, setCart] = useState<IProduct[]>([]);
+  const [totalCart, setTotalCart] = useState<number>(0);
+  const [userData, setUserData] = useState<IUserSession>();
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const storedCart = JSON.parse(localStorage.getItem("cart") || "[]");
+      if (storedCart) {
+        let totalCart = 0;
+        storedCart?.map((item: IProduct) => {
+          totalCart = totalCart + item.price;
+        });
+        setTotalCart(totalCart);
+        setCart(storedCart);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const userData = JSON.parse(localStorage.getItem("userSession")!);
+      setUserData(userData);
+    }
+  }, []);
+
+  const handleClick = async () => {
+    const idProducts = cart?.map((product) => product.id);
+    await createOrder(idProducts, userData?.token!);
+    Swal.fire({
+      title: "Order created successfully!",
+      icon: "success",
+    });
+    setCart([]);
+    setTotalCart(0);
+    localStorage.setItem("cart", "[]");
+  };
+
+  return (
+    <div className="flex flex-row items-center justify-between w-full p-4">
+      <div>
+        {cart && cart.length > 0 ? (
+          cart?.map((cart: IProduct) => {
+            return (
+              <div key={cart.id}>
+                <div>
+                  <img src={cart.image} alt={"Product image" + cart.name} />
+                  <p>{cart.name}</p>
+                  <p>Price: ${cart.price}</p>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <p>Your cart is empty</p>
+        )}
+      </div>
+      <div>
+        <p>Total: ${totalCart}</p>
+        <button onClick={handleClick}>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
 export default CartView;
